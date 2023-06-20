27043189bd5ca2b00fdd1ccbc2efb39b181e4a86
Merlijn Wajer <merlijn@wizzup.org>
2023-06-20 21:29:46 +0200

    tests: add a split+combine test

---
+++ tests/test_hocr_split_recombine.py
@@ -0,0 +1,27 @@
+import pytest
+from subprocess import check_output, check_call
+from os.path import dirname, join
+
+import json
+
+@pytest.mark.usefixtures('sim_hocr_file')
+
+def test_split_combine(sim_hocr_file):
+    sim_hocr_file = str(sim_hocr_file)
+    basedir = dirname(sim_hocr_file)
+
+    split_pages = join(basedir, 'split-%06d.html')
+    split_pages_g = join(basedir, 'split-*.html')
+    combined_file = join(basedir, 'combined.html')
+
+    check_call(['hocr-split-pages', '-f', sim_hocr_file, '-o', split_pages])
+    combined_text = check_output(['hocr-combine-stream', '-g', split_pages_g])
+
+    with open(combined_file, 'wb+') as f:
+        f.write(combined_text)
+
+    plaintext = check_output(['hocr-text', '-f', sim_hocr_file])
+    new_plaintext = check_output(['hocr-text', '-f', combined_file])
+
+    assert plaintext == new_plaintext
+
