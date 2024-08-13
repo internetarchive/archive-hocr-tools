6c00fe18b01999ef48cc1ee65a088598fb85bec9
Scott Barnes <scottreidbarnes@gmail.com>
2024-08-13 15:59:30 -0700

    Fix: use iso-639 rather than iso639
    Owing to a conflict with `archive-ocr-tools`, use `iso-639==0.4.5`
    
    Also of note, DAISY requires two character language codes. See, e.g.
    `dc:Language` at https://www.daisy.org/z3986/2005/Z3986-2005.html#DCM

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='archive-hocr-tools',
       include_package_data=True,
       install_requires=[],
       extras_require={
-          'daisy': ['iso639==0.1.4'],
+          'daisy': ['iso-639==0.4.5'],  # Note this is NOT the `iso639` package used by `epub`.
           'epub': ['ebooklib==0.17.1', 'internetarchive-deriver-module==1.0.1', 'iso639==0.1.4'],
           'pdf': ['PyMuPDF==1.22.5', 'numpy==1.21.3'],
           'pagenumber': ['viterbi-trellis==0.0.3', 'roman>=3.3', 'numpy>=1.21.3', 'scikit-learn>=1.2.2'],
